0a639267af460d2ddb7303d51204d237f2f23146
mukami <mukamihilda69@gmail>
2024-04-23 16:58:35 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import calculator from "./components/calculator";
+import Screen from "./components/Screen";
+import ButtonBox from './components/ButtonBox'
+import Button from './components/Button'
+import CalcProvider from "./context/CalcContext";
+
+const btnValues = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "x"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="],
+];
+
+function App() {
+  return (
+    <CalcProvider>
+      <calculator>
+        <Screen />
+        <ButtonBox>
+          {btnValues.flat().map((btn, i) => (
+            <Button
+              value={btn}
+              key={i}
+            />
+          ))}
+        </ButtonBox>
+      </calculator>
+    </CalcProvider>
+  );
+}
---
+++ src/components/calculator.js
@@ -0,0 +1,9 @@
+import React, { Children } from 'react'
+
+const calculator = () => {
+  return (
+    <div className='calculator'>{Children} </div>
+  )
+}
+
+export default calculator
